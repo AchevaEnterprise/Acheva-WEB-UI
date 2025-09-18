09974c96d2c77aca76d16dd72e6c20adee135a8e
Solemn <davidechoda123@gmail.com>
2025-09-18 10:00:55 +0100

    changed environment variable back to default

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,5 @@ export const environment = {
   production: false,
   staging: false,
   development: true,
-  // BASE_URL: 'https://acheva-nestjs-production.up.railway.app',
-  BASE_URL: 'http://localhost:3000',
+  BASE_URL: 'https://acheva-nestjs-production.up.railway.app',
 };
