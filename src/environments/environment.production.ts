fe1f42118ab7ef31be5906510a91c548d09a20f8
Solemn <davidechoda123@gmail.com>
2025-09-24 12:31:07 +0100

    changed environment url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   staging: false,
   development: false,
-  BASE_URL: 'https://acheva-nestjs-production.up.railway.app',
+  BASE_URL: 'https://acheva-nestjs-staging.up.railway.app',
 };
